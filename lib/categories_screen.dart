1309f71dc8e854d88649472d4f38eab5814b13ba
AmmarJT <AmmarTashk@gmail.com>
2022-07-31 15:41:03 +0300

    Lesson 1: Make ur first screen + Change the startign screen + Make the widget a screen + Navigation and Routing!

---
+++ lib/categories_screen.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+import './dummy_data.dart';
+import './category_item.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  // const CategoriesScreen({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Delimeal'),
+      ),
+      body: GridView(
+        padding: const EdgeInsets.all(25),
+        // children: <Widget>[],
+        children: DUMMY_CATEGORIES
+          .map((cateData) => CategoryItem(
+            cateData.title, 
+            cateData.color,
+            )
+          ).toList(),
+
+        //sliver:(شظية) is a scrollable space in flutter 
+        //delegate: (يفوض) 
+        //main axis: (المحور الاساسي، عشان اتجاه القريد على تحت) same as height
+        //cross axis: (المحور المتقاطع، عشان اتجاه القريد على تحت فدا يتقاطع مع الاتجاه) same as width
+        //Extent:(مدى)
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200, //width
+          childAspectRatio: 3 / 2, //so if width is 200 height will be 300
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+
+
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,86 @@
+// Lesson 1: Make ur first screen + Change the startign screen + Make the widget a screen + Navigation and Routing!
+
+// Make ur first screen + set up the project:
+//    - just make a new widget called categories_screen
+//    - put a GridView inside it
+//    - make a class called category in model folder (to make every category an object)
+//    - make category_item widget (which will be under the categories_screen)
+//    - put the dummy_data.dart in the lib 
+//    - In categroies_screen: import dummy + category_item and use them!
+
+
+// Change the startign screen:
+//    - in MaterialApp(home: هنا) <<<< put the starting screen widget
+
+
+// Make the widget a screen
+//    - Screen / widget / page === all the same
+//    - Typically the Scaffold represent a screen, so you'll use it in every screen
+//    - Put the Scaffold in categories_screen, and put the gridView inside it
+//    - complete the Scaffold set up: appBar, .....etc
+//    - style the screen and its children
+
+
+// Navigation and Routing:
+//    - make a new widget screen called category_meals_screen
+//    - make it a screen by wraping the content in Scaffold
+//    - make the category_item tappable using: GestureDetector or InkWell (GestureDetector with effects))
+//    - write a function for selectCategory and trigger it using onTap of the InkWell
+//    - use Navigator + MaterialPageRoute : in the selectCategory() function!
+//    - Congrats!!! for your first navigation and routing ever!!
+
+
 import 'package:flutter/material.dart';
+import 'package:meal_app/categories_screen.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'DeliMeals',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.pink,
+        accentColor: Colors.amber,
+        canvasColor: Color.fromRGBO(255, 254, 229, 1),
+        fontFamily: 'Raleway',
+        textTheme: ThemeData.light().textTheme.copyWith(
+          bodySmall: TextStyle(
+            color: Color.fromRGBO(20, 51, 51, 1)
+          ),
+          bodyMedium: TextStyle(
+            color: Color.fromRGBO(20, 51, 51, 1)
+          ),
+          titleMedium: TextStyle(
+            fontSize: 20,
+            fontFamily: 'RobotCondensed',
+            fontWeight: FontWeight.bold
+          )
+        )
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: CategoriesScreen(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
+// class MyHomePage extends StatefulWidget {
+//   @override
+//   State<MyHomePage> createState() => _MyHomePageState();
+// }
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
-}
+// class _MyHomePageState extends State<MyHomePage> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('DeliMeals'),
+//       ),
+//       body: Center(
+//         child: Text('Navigation Time!'),
+//       ),
+//     );
+//   }
+// }
